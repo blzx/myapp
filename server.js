29c69f31cbdde5c49b1b3fae246a7aa2f4b844b7
blzx <2511026858@qq.com>
2019-07-07 19:51:40 +0800

    配置nginx

--- server.js
+++ server.js
@@ -54,11 +54,13 @@ fs.readdir('./testData',function(err,files){
         })
     }
 });
-app.use(function(req, res, next) {
+
+// 允许跨域
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+});*/
 app.listen('8888');
